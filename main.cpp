bf2208801ebca6a45332731a81047100b7052bc5
phen0menon <garaganda21@gmail.com>
2019-10-07 20:25:34 +0300

    Use 4 spaces instead of 2

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 
 int main() {
-  std::vector<int> collection;
+    std::vector<int> collection;
 
-  for (unsigned int i = 0, num; i < 10; i++) {
-    std::cin >> num;
-    collection.push_back(num);
-  }
+    for (unsigned int i = 0, num; i < 10; i++) {
+        std::cin >> num;
+        collection.push_back(num);
+    }
 
-  std::sort(collection.begin(), collection.end());
-
-  return 0;
+    std::sort(collection.begin(), collection.end());
+    return 0;
 }
